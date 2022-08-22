40ffcc23a77bb64e988179d0e16134cfc46a3538
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-08-22 17:58:00 +0600

    footer completed

--- components/Shared/Footer.js
+++ components/Shared/Footer.js
@@ -11,67 +11,74 @@ import {
 
 const Footer = () => {
   return (
-    <div className="bg-[#0F192D] py-20">
-      <div className="sm:w-[90%] mx-auto flex flex-col sm:flex-row">
-        <div className="border border-green-600 basis-full sm:basis-1/2">
+    <div className="bg-[#0F192D] pt-16">
+      <div className="sm:w-[90%] mx-auto flex flex-wrap">
+        <div className="px-3 basis-full lg:basis-1/2">
           <h2 className="text-white text-6xl">E</h2>
-          <p className="text-white">
+          <p className="text-white text-xs sm:text-sm">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
           </p>
 
-          <div className="mt-24">
-            <div className="border border-red-500 flex flex-col sm:flex-row">
+          <div className="mt-8">
+            <div className="flex flex-wrap gap-[10px]">
               {/* newsletter  */}
-              <div className="border pr-3 sm:grow">
-                <h2 className="text-3xl text-white">Get Our Newsletter</h2>
-                <p className="mt-3 text-white">
+              <div className="basis-full sm:basis-[calc(65%-5px)]">
+                <h2 className="text-xl sm:text-3xl text-white">
+                  Get Our Newsletter
+                </h2>
+                <p className="mt-3 text-white text-xs sm:text-sm">
                   Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 </p>
               </div>
 
               {/* socil icons  */}
-              <div className="border border-green-600 flex gap-3 mt-2 sm:mt-2 sm:self-end">
-                <FaFacebookSquare className="text-gray-600 cursor-pointer text-3xl" />
-                <FaInstagram className="text-gray-600 cursor-pointer text-3xl" />
-                <FaTwitter className="text-gray-600 cursor-pointer text-3xl" />
-                <FaYoutube className="text-gray-600 cursor-pointer text-3xl" />
-                <FaLinkedin className="text-gray-600 cursor-pointer text-3xl" />
+              <div className="basis-full sm:basis-[calc(35%-5px)] self-end flex gap-2">
+                <FaFacebookSquare className="text-gray-600 cursor-pointer text-xl sm:text-3xl" />
+                <FaInstagram className="text-gray-600 cursor-pointer text-xl sm:text-3xl" />
+                <FaTwitter className="text-gray-600 cursor-pointer text-xl sm:text-3xl" />
+                <FaYoutube className="text-gray-600 cursor-pointer text-xl sm:text-3xl" />
+                <FaLinkedin className="text-gray-600 cursor-pointer text-xl sm:text-3xl" />
               </div>
             </div>
 
             {/* form  */}
             <div className="mt-8">
-              <div className="flex flex-row flex-wrap">
+              <div className="flex flex-wrap gap-[10px]">
                 <input
                   type="text"
                   placeholder="Enter Email"
-                  className="p-2 border border-gray-700 basis-10/12 sm:basis-6/12 bg-transparent text-white outline-0 focus:border-white"
+                  className="p-2 border-b border-gray-700 basis-full sm:basis-[calc(65%-5px)]  bg-transparent text-white outline-0 focus:border-white placeholder:text-sm"
                 />
-                <button className="bg-blue-400 mt-2 sm:mt-0 p-2 basis-10/12  sm:basis-5/12 uppercase">
+                <button className="bg-blue-400 basis-full sm:basis-[calc(35%-5px)] p-2 uppercase text-xs">
                   Subscribe To Newsletter
                 </button>
               </div>
 
               <div className="mt-8 flex items-center">
                 <input
-                  className="w-[25px] h-[25px]"
+                  className="w-[15px] h-[15px] md:w-[20px] md:h-[20px]"
                   type="checkbox"
                   id="newsletterCheck"
                 />
-                <label htmlFor="newsletterCheck" className="ml-3 text-white">
+                <label
+                  htmlFor="newsletterCheck"
+                  className="ml-3 text-white text-xs md:text-sm"
+                >
                   Yes i'd like to receive bussiness email and agree with{" "}
-                  <a href="/termsandcondition">terms and condition</a>
+                  <Link href="/termsandcondition" passHref>
+                    <a>terms and condition</a>
+                  </Link>
                 </label>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="border border-indigo-500 basis-full sm:basis-1/2 text-white">
-          <div className="flex">
-            <div className="border border-red-500 basis-1/4 px-2">
-              <h4 className="uppercase">company</h4>
-              <ul className="border border-white mt-5 capitalize">
+        <div className="basis-full lg:basis-1/2 mt-5 lg:mt-0 text-white">
+          <div className="flex flex-wrap gap-[10px] sm:gap-[6.67px]">
+            <div className="basis-[calc(50%-5px)] p-3 sm:basis-[calc(25%-5px)]">
+              <h4 className="uppercase text-xs sm:text-sm">company</h4>
+              <ul className="mt-5 capitalize text-xs sm:text-sm">
                 <li>about us</li>
                 <li>policies</li>
                 <li>pricing</li>
@@ -80,9 +87,9 @@ const Footer = () => {
               </ul>
             </div>
 
-            <div className="border border-green-500 basis-1/4 px-2">
-              <h4 className="uppercase">service</h4>
-              <ul className="border border-white mt-5 capitalize">
+            <div className="basis-[calc(50%-5px)] p-3 sm:basis-[calc(25%-5px)]">
+              <h4 className="uppercase text-xs sm:text-sm">service</h4>
+              <ul className=" mt-5 capitalize  text-xs sm:text-sm">
                 <li>works</li>
                 <li>search profiles</li>
                 <li>licensing</li>
@@ -91,9 +98,9 @@ const Footer = () => {
               </ul>
             </div>
 
-            <div className="border border-yellow-500 basis-1/4 px-2">
-              <h4 className="uppercase">learn</h4>
-              <ul className="border border-white mt-5 capitalize">
+            <div className="basis-[calc(50%-5px)] p-3 sm:basis-[calc(25%-5px)]">
+              <h4 className="uppercase text-xs sm:text-sm">learn</h4>
+              <ul className="mt-5 capitalize  text-xs sm:text-sm">
                 <li>UX Design</li>
                 <li>Creative Skills</li>
                 <li>community</li>
@@ -102,9 +109,9 @@ const Footer = () => {
               </ul>
             </div>
 
-            <div className="border border-green-500 basis-1/4 px-2">
-              <h4 className="uppercase">collaborate</h4>
-              <ul className="border border-white mt-5 capitalize">
+            <div className="basis-[calc(50%-5px)] p-3 sm:basis-[calc(25%-5px)]">
+              <h4 className="uppercase text-xs sm:text-sm">collaborate</h4>
+              <ul className="mt-5 capitalize  text-xs sm:text-sm">
                 <li>partner with us</li>
                 <li>become an affiliate</li>
                 <li>FAQ'S</li>
@@ -113,31 +120,35 @@ const Footer = () => {
               </ul>
             </div>
           </div>
-          <div className="mt-16">
-            <ul className="border border-indigo-600 flex flex-wrap">
-              <li className="border-r-2 border-gray-400 pr-2 text-sm uppercase">
+
+          <div className="mt-10 p-3">
+            <ul className="flex text-xs">
+              <li className="border-r-2 grow border-gray-400 pr-2 uppercase">
                 <Link href="#">Terms of bussiness</Link>
               </li>
-              <li className="border-r-2 border-gray-400 px-2 text-sm uppercase">
+              <li className="border-r-2 grow border-gray-400 px-2  uppercase">
                 <Link href="#">data usage &amp; privacy policy</Link>
               </li>
-              <li className="border-r-2 border-gray-400 px-2 text-sm uppercase">
+              <li className="border-r-2 grow border-gray-400 px-2  uppercase">
                 <Link href="#">cancellation policy</Link>
               </li>
-              <li className="border-r-2 border-gray-400 px-2 text-sm uppercase">
+              <li className="border-r-2 grow border-gray-400 px-2  uppercase">
                 <Link href="#">our ethics statement</Link>
               </li>
-              <li className="pl-2 text-sm uppercase">
+              <li className="pl-2 grow uppercase">
                 <Link href="#">sitemap</Link>
               </li>
             </ul>
-            <p className="mt-6">
+
+            <p className="mt-6 text-xs sm:text-sm">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Perspiciatis aliquam cumque facilis cupiditate similique excepturi
               voluptates dolor eum a. Iste adipisci animi mollitia nobis, quis
               deleniti expedita quam laudantium harum.
             </p>
-            <p className="mt-4">Lorem ipsum dolor sit amet.</p>
+            <p className="mt-4 text-xs sm:text-sm">
+              Lorem ipsum dolor sit amet.
+            </p>
           </div>
         </div>
       </div>
